d9579e13499c09a8b7793697f8ff335f3ac27aba
jun1-per <jun1per@tutamail.com>
2024-07-31 23:20:53 +0300

    Fixed typo in docs

---
+++ Eleonora.Core/Services/ServiceComputerVision.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Eleonora.Core.Services
+{
+    public class ServiceComputerVision
+    {
+        const string subscriptionKey = "";
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+        public static async Task<string> MakeAnalysisRequest(Stream stream)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                // Request headers.
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+                // Request parameters. A third optional parameter is "details".
+                string requestParameters = "visualFeatures=Categories,Description,Color&language=en";
+
+                // Assemble the URI for the REST API Call.
+                string uri = uriBase + "?" + requestParameters;
+
+                HttpResponseMessage response;
+
+                byte[] byteData;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    byteData = ms.ToArray();
+                }
+                string result = string.Empty;
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    // This example uses content type "application/octet-stream".
+                    // The other content types you can use are "application/json" and "multipart/form-data".
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                    // Execute the REST API call.
+                    response = await client.PostAsync(uri, content);
+
+                    // Get the JSON response.
+                    result = await response.Content.ReadAsStringAsync();
+                }
+                return result;
+            } catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
